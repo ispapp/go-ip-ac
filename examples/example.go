c4889dbea784e47da19088297f4e46aa5b6acde2
andrewhodel <andrewhodel@gmail.com>
2022-10-10 07:27:11 +0000

    fixed example, now using references

--- examples/example.go
+++ examples/example.go
@@ -7,28 +7,28 @@ import (
 
 func main() {
 
-	var opts ipac.Ipac
-	var ip_ac = ipac.Init(opts)
+	var ip_ac ipac.Ipac
+	ipac.Init(&ip_ac)
 
 	// set authorization status for an IP
 	// reset (use this on invalid authorization attempts)
-	ipac.ModifyAuth(ip_ac, 0, "127.0.0.1")
+	ipac.ModifyAuth(&ip_ac, 0, "127.0.0.1")
 	// failed/unauthorized (use this on valid logouts)
-	ipac.ModifyAuth(ip_ac, 1, "127.0.0.1")
+	ipac.ModifyAuth(&ip_ac, 1, "127.0.0.1")
 	// authorized (use this on valid logins)
-	ipac.ModifyAuth(ip_ac, 2, "127.0.0.1")
+	ipac.ModifyAuth(&ip_ac, 2, "127.0.0.1")
 
 	// test authorization status for an IP
 	// this needs to be called every time there is a new IP connection
-	var status = ipac.TestIpAllowed(ip_ac, "127.0.0.1")
+	var status = ipac.TestIpAllowed(&ip_ac, "127.0.0.1")
 	fmt.Printf("TestIpAllowed 127.0.0.1: %t\n", status)
 
 	// test if you should warn users from an IP
-	var warn = ipac.TestIpWarn(ip_ac, "127.0.0.1")
+	var warn = ipac.TestIpWarn(&ip_ac, "127.0.0.1")
 	fmt.Printf("TestIpWarn 127.0.0.1: %t\n", warn)
 
 	// return details for a specific ip address
-	var ip_details = ipac.IpDetails(ip_ac, "127.0.0.1")
+	var ip_details = ipac.IpDetails(&ip_ac, "127.0.0.1")
 	fmt.Printf("IpDetails 127.0.0.1: %+v\n", ip_details)
 
 	select{}
